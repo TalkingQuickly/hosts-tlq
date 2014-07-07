c44cb73d936c16da1cf25027518d56fea003a0d2
Ben Dixon <ben@hillsbede.co.uk>
2014-07-07 12:11:55 +0100

    Fixes cookbook naming to match repo

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             "hosts"
+name             "hosts-tlq"
 maintainer       "Ben Dixon"
 maintainer_email "ben@talkingquickly.co.uk"
 description      "Add hosts from data bag to hosts file"
 depends          'hostsfile'
-version           "0.0.1"
+version           "0.0.2"
